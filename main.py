94aa7c29ef55e2fde258106207abf86e76c5b9dd
William Gardner <william@madebycounter.com>
2024-08-21 11:23:25 -0700

    Added mouse step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from showfile import Showfile
-from format import Hex, String
 from macro import Macro
+import steps
 
 IN_FILE = "L:\\MagicQ\\MacroTest5.shw"
 OUT_FILE = "L:\\MagicQ\\FileWriteTest.shw"
@@ -12,8 +12,13 @@ for i in range(len(show.data)):
     if Macro.verify(show.data[i]):
         macro = Macro.parse(show.data[i])
 
-        for step in macro.steps:
-            print(step)
+        for j in range(len(macro.steps)):
+            if steps.Mouse.verify(macro.steps[j]):
+                step = steps.Mouse.parse(macro.steps[j])
+                step.x = 99
+                step.y = 99
+                macro.steps[j] = steps.Mouse.format(step)
 
+        show.data[i] = Macro.format(macro)
 
 show.write(OUT_FILE)
---
+++ steps.py
@@ -0,0 +1,41 @@
+from format import Hex
+
+
+class Mouse:
+    CODE = "0009"
+    PRESSED = 1
+    RELEASED = 0
+
+    def __init__(self, time=1, var1="", x=0, y=0, direction=PRESSED):
+        self.time = time
+        self.var1 = var1
+        self.x = x
+        self.y = y
+        self.direction = direction
+
+    def __repr__(self):
+        direction = "PRESSED" if self.direction == Mouse.PRESSED else "RELEASED"
+        return "Mouse<({}, {}), {}>".format(self.x, self.y, direction)
+
+    @staticmethod
+    def verify(line):
+        return line[1] == Mouse.CODE
+
+    @staticmethod
+    def parse(line):
+        time = Hex.parse(line[0])
+        x = Hex.parse(line[3][0:4])
+        y = Hex.parse(line[3][4:8])
+        direction = Hex.parse(line[4])
+
+        return Mouse(time=time, x=x, y=y, direction=direction, var1=line[2])
+
+    @staticmethod
+    def format(mouse):
+        return [
+            Hex.format(mouse.time, fill=8),
+            Mouse.CODE,
+            mouse.var1,
+            Hex.format(mouse.x) + Hex.format(mouse.y),
+            Hex.format(mouse.direction, fill=8),
+        ]
